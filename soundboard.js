e508c1f209218fc7636b475096980a4cc3db4218
BlitzKraig <craig@blitzkraig.com>
2021-09-23 15:51:16 +0100

    Add directory picker for sb dir setting

--- soundboard.js
+++ soundboard.js
@@ -677,13 +677,15 @@ class SoundBoard {
             hint: game.i18n.localize('SOUNDBOARD.settings.directory.hint'),
             scope: 'world',
             config: true,
-            default: 'modules/SoundBoard/exampleAudio/',
+            default: 'modules/SoundBoard/exampleAudio',
             onChange: value => {
                 if (value.length <= 0) {
-                    game.settings.set('SoundBoard', 'soundboardDirectory', 'modules/SoundBoard/exampleAudio/');
+                    game.settings.set('SoundBoard', 'soundboardDirectory', 'modules/SoundBoard/exampleAudio');
                 }
                 SoundBoard.getSounds();
-            }
+            },
+            type: String,
+            filePicker: true
         });
 
         game.settings.register('SoundBoard', 'source', {
