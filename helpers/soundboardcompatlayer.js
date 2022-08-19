a5a1405e417c94177ceb27db2c4bd51a7e160546
BlitzKraig <craig@blitzkraig.com>
2022-08-19 15:36:54 +0100

    Add compat layer

---
+++ helpers/soundboardcompatlayer.js
@@ -0,0 +1,18 @@
+class SBCompatLayer {
+
+    static getKeyDown(keyCode, alternateKeyCode = undefined) {
+        if(Math.floor(game.version) < 10){
+            return keyboard._downKeys.has(keyCode);
+        } else {
+            return keyboard.downKeys.has(alternateKeyCode ?? keyCode);
+        }
+    }
+
+    static async deleteMacros(macros) {
+        if(Math.floor(game.version) < 10){
+            return await Macro.delete(macros);
+        } else {
+            return await Macro.deleteDocuments(macros);
+        }
+    }
+}
